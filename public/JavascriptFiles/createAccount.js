4ba7a2cf2fb1b3f346266294c95b2ed8fd2271df
Levente Székely <31960469+player1s@users.noreply.github.com>
2019-05-19 23:55:41 +0200

    progress, continue at updating the counter for documents in collection

---
+++ public/JavascriptFiles/createAccount.js
@@ -0,0 +1,107 @@
+window.onload=function() {
+    const firestore = firebase.firestore();
+    const fieldEmail = document.getElementById('createAccountFieldEmail');
+    const fieldPassword = document.getElementById('createAccountFieldPassword');
+    const createAccountBtnCreateAccount = document.getElementById('createAccountBtnCreateAccount');
+    const createAccountFieldName = document.getElementById('createAccountFieldName');
+    const createAccountRadioStudent = document.getElementById('createAccountRadioStudent');
+    const createAccountRadioEducationalInstitute = document.getElementById('createAccountRadioEducationalInstitute');
+    const createAccountRadioCompany = document.getElementById('createAccountRadioCompany');
+    const createAccountFeedback = document.getElementById('createAccountFeedback');
+    let size = null;
+
+    createAccountBtnCreateAccount.addEventListener("click", function () {
+        console.log("in: click eventlistener for btnCreateAccount");
+        const email = fieldEmail.value;
+        const pass = fieldPassword.value;
+        const auth = firebase.auth();
+        let num = null;
+        let myData = null;
+
+        firestore.collection("Companies").doc("1").get().then(function (doc) {
+            if (doc && doc.exists) {
+                myData = doc.data();
+                num = myData.currentAmountOfDocs + 2;
+                console.log("Hot dog status: " + myData.currentAmountOfDocs);
+
+            }
+        });
+
+        //firestore.collection("Companies").doc("1").set({currentAmountOfDocs: num});
+
+/*
+        const promise = auth.createUserWithEmailAndPassword(email, pass).then( function () {
+
+                if(document.getElementById("createAccountRadioCompany").checked)
+                {
+
+                     firestore.collection("Companies").doc("1").set({currentAmountOfDocs: firebase.firestore.FieldValue.increment(1)});
+                    console.log("in: click eventlistener for btnCreateAccount");
+
+                    let docRef = firestore.collection("Companies").doc(size+1);
+                    const textToSave = createAccountFieldName.value;
+                    docRef.set({name: textToSave}).then(function () {
+                        console.log("status saved!");
+                    }).catch(function (error) {
+                        console.log("Got error ", error);
+                    })
+
+
+
+                }
+
+        });
+        promise.catch(function(e) {
+            createAccountFeedback.innerText = e.message;
+            console.log(e.message);
+        });
+
+ */
+    });
+
+
+
+
+
+
+
+
+
+        /* fires all the time
+
+
+        saveBtn.addEventListener("click", function () {
+            console.log("in: click eventlistener for savebtn");
+            docRef = firestore.collection("Companies").doc("1");
+            const textToSave = inputTextField.value;
+            console.log("saving this to firestore: " + textToSave);
+            docRef.set({name: textToSave}).then(function () {
+                console.log("status saved!");
+            }).catch(function (error) {
+                console.log("Got error ", error);
+            })
+        });
+
+
+
+
+
+
+
+
+
+                    firebase.auth().onAuthStateChanged(function(user) {
+                        if (user) {
+                            user.sendEmailVerification().then(function () {
+                                console.log("email sent")
+                            }).catch(function (error) {
+                                console.log("verification email error " + error)
+                            });
+                        }
+
+                    })
+
+         */
+
+
+};
