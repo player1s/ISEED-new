acba4f00e2505d7a04b85912c84fe818c8a52a7a
Levente Székely <31960469+player1s@users.noreply.github.com>
2019-05-19 21:45:25 +0200

    Login page added (devoid of styling)

--- public/JavascriptFiles/app.js
+++ public/JavascriptFiles/app.js
@@ -1,15 +1,4 @@
 
-    var firebaseConfig = {
-        apiKey: "AIzaSyA5WwCvhoJv6-3G7VfmVwI0HcRWFtiSipQ",
-        authDomain: "iseed-d26e9.firebaseapp.com",
-        databaseURL: "https://iseed-d26e9.firebaseio.com",
-        projectId: "iseed-d26e9",
-        storageBucket: "iseed-d26e9.appspot.com",
-        messagingSenderId: "275576830551",
-        appId: "1:275576830551:web:bf48eebf3862a8bf"
-    };
-// Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
 
 
 var firestore = firebase.firestore();
