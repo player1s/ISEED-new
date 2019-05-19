7429b7461dfcbd606d1ee764ac2ae6d1f09272a8
Levente Székely <31960469+player1s@users.noreply.github.com>
2019-05-19 21:49:02 +0200

    fix on login

--- public/JavascriptFiles/Login.js
+++ public/JavascriptFiles/Login.js
@@ -1,25 +1,25 @@
-const fieldEmail = document.getElementById('fieldEmail');
-const fieldPassword = document.getElementById('fieldPassword');
-const btnLogin = document.getElementById('btnLogin');
-const pFeedback = document.getElementById('feedback');
+window.onload=function() {
+    const fieldEmail = document.getElementById('fieldEmail');
+    const fieldPassword = document.getElementById('fieldPassword');
+    const btnLogin = document.getElementById('btnLogin');
+    const pFeedback = document.getElementById('feedback');
 
-btnLogin.addEventListener("click", function () {
-    console.log("in: click eventlistener for btnLogin");
-    const email = fieldEmail.value;
-    const pass = fieldPassword.value;
-    const auth = firebase.auth();
+    btnLogin.addEventListener("click", function () {
+        console.log("in: click eventlistener for btnLogin");
+        const email = fieldEmail.value;
+        const pass = fieldPassword.value;
+        const auth = firebase.auth();
 
-    const promise = auth.signInWithEmailAndPassword(email, pass);
-    promise.catch(e => pFeedback.innerText = e.message);
+        const promise = auth.signInWithEmailAndPassword(email, pass);
+        promise.catch(e => pFeedback.innerText = e.message);
 
-});
+    });
 
-firebase.auth().onAuthStateChanged(firebaseUser => {
-    if(firebaseUser) {
-        console.log("this user logged in: " + firebaseUser);
-        document.getElementById('btnLogout').removeAttribute("hidden");
-    } else {
-        console.log("not logged in");
-        document.getElementById('btnLogout').setAttribute("hidden", "hidden");
-    }
-});
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if (firebaseUser) {
+            console.log("this user logged in: " + firebaseUser);
+        } else {
+            console.log("not logged in");
+        }
+    });
+}
