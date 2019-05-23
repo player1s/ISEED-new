0ee33403c8cd6cbf19556a4a5ad7f79133123580
Levente Székely <31960469+player1s@users.noreply.github.com>
2019-05-23 17:31:34 +0200

    Login to the correct pages

--- public/JavascriptFiles/login.js
+++ public/JavascriptFiles/login.js
@@ -3,6 +3,9 @@ window.onload=function() {
     const fieldPassword = document.getElementById('fieldPassword');
     const btnLogin = document.getElementById('btnLogin');
     const pFeedback = document.getElementById('feedback');
+    let firestore = firebase.firestore();
+    let docRef = null;
+    let type = null;
 
     btnLogin.addEventListener("click", function () {
         console.log("in: click eventlistener for btnLogin");
@@ -11,17 +14,32 @@ window.onload=function() {
         const auth = firebase.auth();
 
         const promise = auth.signInWithEmailAndPassword(email, pass).then( function () {
-            window.location.href="loggedInStudent.html";
+            const user = firebase.auth().currentUser;
+            docRef = firestore.collection("Users").doc(user.uid);
+
+            docRef.get().then(function (doc) {
+
+                console.log(doc);
+                console.log(doc.exists);
+
+                if (doc && doc.exists) {
+                    const myData = doc.data();
+                    type = myData.type;
+                    console.log(type);
+                }
+
+            if(type.localeCompare("Student") === 0) {
+                window.location.href = "loggedInStudent.html";
+            }
+            if(type.localeCompare("EducationalInstitute") === 0) {
+                window.location.href = "loggedInEducationalInstitute.html";
+            }
+            if(type.localeCompare("Company") === 0) {
+                window.location.href = "loggedInCompany.html";
+            }
+            });
         });
         promise.catch(e => pFeedback.innerText = e.message);
 
     });
-
-    firebase.auth().onAuthStateChanged(firebaseUser => {
-        if (firebaseUser) {
-            console.log("this user logged in: " + firebaseUser);
-        } else {
-            console.log("not logged in");
-        }
-    });
-}
+};
