16dbea07449c1ca6082bed6f196a44b4dce2ad61
Levente Székely <31960469+player1s@users.noreply.github.com>
2019-05-23 19:24:10 +0200

    preparation for getting all cases for students

--- public/JavascriptFiles/loggedInStudent.js
+++ public/JavascriptFiles/loggedInStudent.js
@@ -1,10 +1,25 @@
 window.onload=function() {
     const btnLogout = document.getElementById('btnLogout');
+    const CaseShow = document.getElementById('CaseShow');
 
     btnLogout.addEventListener("click", function () {
         console.log("in: click eventlistener for btnLogout");
         firebase.auth().signOut().then(function () {
             window.location.href = "login.html";
         })
+    });
+
+};
+
+getRealtimeUpdates = function () {
+    let firestore = firebase.firestore();
+    let docRef = firestore.collection("Cases").where("*");
+
+    docRef.onSnapshot(function (doc) {
+        if (doc && doc.exists) {
+            const myData = doc.data();
+            CaseShow.innerText = myData.name;
+
+        }
     });
 };
