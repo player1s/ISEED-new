3cc8bf7c0a84d2695d32df901b824b18b06efd57
Levente Székely <31960469+player1s@users.noreply.github.com>
2019-09-11 12:23:57 +0200

    restart

--- public/JavascriptFiles/loggedInStudent.js
+++ public/JavascriptFiles/loggedInStudent.js
@@ -2,12 +2,31 @@ window.onload=function() {
     const btnLogout = document.getElementById('btnLogout');
     const CaseShow = document.getElementById('CaseShow');
 
+    let db = firebase.firestore();
+
+
+    var docRef = db.collection("Cases").doc("1566835055609");
+
+    docRef.get().then(function(doc) {
+        if (doc.exists) {
+            console.log("Document data:", doc.data().name);
+            CaseShow.innerText = doc.data().name;
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+
     btnLogout.addEventListener("click", function () {
         console.log("in: click eventlistener for btnLogout");
         firebase.auth().signOut().then(function () {
             window.location.href = "login.html";
         })
     });
+
+
 
 };
 
