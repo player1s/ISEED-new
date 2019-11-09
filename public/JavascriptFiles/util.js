0883c383d1a646b40f6af330e594d2f5733fb012
Levente Székely <31960469+player1s@users.noreply.github.com>
2019-11-09 18:18:05 +0100

    logout buttons!

---
+++ public/JavascriptFiles/util.js
@@ -0,0 +1,45 @@
+window.onload=function() {
+    const btnLogout = document.getElementById('btnLogout');
+    /*   const CaseShow = document.getElementById('CaseShow');
+       let db = firebase.firestore();
+
+
+       var docRef = db.collection("Cases").doc("1566835055609");
+
+       docRef.get().then(function(doc) {
+           if (doc.exists) {
+               console.log("Document data:", doc.data().name);
+               CaseShow.innerText = doc.data().name;
+           } else {
+               // doc.data() will be undefined in this case
+               console.log("No such document!");
+           }
+       }).catch(function(error) {
+           console.log("Error getting document:", error);
+       });
+   */
+    btnLogout.addEventListener("click", function () {
+        console.log("in: click eventlistener for btnLogout");
+        firebase.auth().signOut().then(function () {
+            window.location.href = "login.html";
+        })
+    });
+
+
+
+};
+/*
+getRealtimeUpdates = function () {
+    let firestore = firebase.firestore();
+    let docRef = firestore.collection("Cases").where("*");
+
+    docRef.onSnapshot(function (doc) {
+        if (doc && doc.exists) {
+            const myData = doc.data();
+            CaseShow.innerText = myData.name;
+
+        }
+    });
+};
+
+ */
